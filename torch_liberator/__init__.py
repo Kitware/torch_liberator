b63128d9805e8cdc8c9cbac92a26a64b5757d794
joncrall <erotemic@gmail.com>
2021-05-06 11:39:56 -0400

    Expose load partial state in public API

--- torch_liberator/__init__.py
+++ torch_liberator/__init__.py
@@ -9,6 +9,7 @@ from torch_liberator import exporter
 
 from torch_liberator.deployer import (DeployedModel, deploy,)
 from torch_liberator.exporter import (export_model_code,)
+from torch_liberator.initializer import (load_partial_state,)
 
 __all__ = ['DeployedModel', 'deploy', 'deployer', 'export_model_code',
-           'exporter']
+           'exporter', 'load_partial_state']
