0f49104a9499a8251b5f43b47ccfdc41d00c7081
joncrall <erotemic@gmail.com>
2021-11-01 00:02:53 -0400

    Start branch for 0.1.2

--- torch_liberator/__init__.py
+++ torch_liberator/__init__.py
@@ -2,7 +2,7 @@
 mkinit ~/code/torch_liberator/torch_liberator/__init__.py -w
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from torch_liberator import deployer
 from torch_liberator import exporter
