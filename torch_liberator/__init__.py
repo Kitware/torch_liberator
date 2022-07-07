3b5027cd48dafd100aea4e3a1d0068037ad87cd4
joncrall <erotemic@gmail.com>
2022-07-07 00:26:47 -0400

    try to force a fix

--- torch_liberator/__init__.py
+++ torch_liberator/__init__.py
@@ -2,7 +2,7 @@
 mkinit ~/code/torch_liberator/torch_liberator/__init__.py -w
 """
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from torch_liberator import deployer
 from torch_liberator import exporter
